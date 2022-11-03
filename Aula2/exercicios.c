8c846cc9ccbe872625850465aaef83104191f78f
Lorenzo Malavolta <lorenzo.malavolta@rodosoft.com.br>
2022-11-03 11:35:06 -0300

    primeiro commit

---
+++ Aula2/exercicios.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int exercicios() {
+	//1 - Faça um algoritmo que mostre na tela seu nome. Compile e veja o resultado
+	printf("\n\nExercicio 1:\n");
+	printf("Nome: Lorenzo");
+	
+	/*2 - Faça um algoritmo que mostre na tela seu nome, idade e endereço. 
+	Compile e veja o resultado*/
+	printf("\n\nExercicio 2:\n");
+	printf("Nome: Lorenzo\n");
+	printf("Idade: 22 anos\n");
+	printf("Endereço: Rua teste\n");
+	
+	//4- Faça um algoritmo que apresente na tela o menu abaixo. Compile e veja o resultado.
+	printf("\n\nExercicio 4:\n");
+	printf("--------------------------------------------\n");
+	printf("1.Salvar\n");
+	printf("2.Cadastrar\n");
+	printf("3.Sair\n");
+	printf("--------------------------------------------\n");
+	return 0;
+}
